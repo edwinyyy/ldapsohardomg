9146537a20e474240b68e8b7d59bc76c998848c6
LAPTOP-QPLF78RP\acer <chotiphatwin@gmail.com>
2024-04-25 20:25:03 +0700

    change title

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (isset($_GET['error']) && $_GET['error'] == 'auth_fail') {
     input{ padding: 10px; font-size: 20;}
 </style></head>
 </body>
-<h1>Authentication with Active Directory</h1>
+<h1>Fuck this project</h1>
 <form action="Auth_test.php" method="post">
     <input type = "text" name= "username" /><br>
     <input type = "password" name= "password" /><br>
